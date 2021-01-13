675d02518a8fbf944b1af2433e1b16519ec3ab87
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-01-13 14:19:22 -0300

    Paginador y Filtros - Alertas

--- controllers/tareas.controllers.js
+++ controllers/tareas.controllers.js
@@ -34,10 +34,42 @@ const listarTareas = async (req, res) => {
 
 // Listar tareas por vencer/vencidas
 const tareasVencidas = async (req, res) => {
-    
+
     const hoy = moment().format('YYYY-MM-DD');
     const manana = moment().add(1, 'days').format('YYYY-MM-DD');
-    let condicion = {};
+
+    let paginador = { 
+        desdeTareas: 0, 
+        hastaTareas: 0,
+        desdeVencidas: 0, 
+        hastaVencidas: 0,
+        desdePorVencers: 0, 
+        hastaPorVencers: 0, 
+    };
+
+    paginador.desdeTareas = Number(req.query.desdeTareas) || 0;
+    paginador.hastaTareas = Number(req.query.hastaTareas) || 0;
+    paginador.desdeVencidas = Number(req.query.desdeVencidas) || 0;
+    paginador.hastaVencidas = Number(req.query.hastaVencidas) || 0;
+    paginador.desdePorVencer = Number(req.query.desdePorVencer) || 0;
+    paginador.hastaPorVencer = Number(req.query.hastaPorVencer) || 0;
+    
+    
+    let busquedaPorVencer = {activo: true};
+    let busquedaVencidas = {activo: true};
+    
+    const descripcionPorVencer = req.query.descripcionPorVencer || '';
+    const descripcionVencidas = req.query.descripcionVencidas || '';
+    
+    if(descripcionPorVencer){
+        const regex = new RegExp(descripcionPorVencer, 'i'); // Expresion regular para busqueda insensible
+        busquedaPorVencer.descripcion= regex;
+    }
+
+    if(descripcionVencidas){
+        const regex = new RegExp(descripcionVencidas, 'i'); // Expresion regular para busqueda insensible
+        busquedaVencidas.descripcion = regex;
+    }
 
     const condicionTareas = { fecha_limite: { $lte: new Date(`${manana}T00:00:00.000Z`)} }
     const condicionPorVencer = { fecha_limite:{ $gte: new Date(`${hoy}T00:00:00.000Z`), $lt: new Date(`${manana}T00:00:00.000Z`) } }
@@ -45,12 +77,28 @@ const tareasVencidas = async (req, res) => {
     
     try{
         const [tareas, vencidas, porVencer, totalTareas, totalPorVencer, totalVencidas] = await Promise.all([
-            Tarea.find({activo: true}).where(condicionTareas).populate('plaza', 'descripcion').sort({plaza: -1}),        
-            Tarea.find({activo: true}).where(condicionVencidas).populate('plaza', 'descripcion').sort({plaza: -1}),         
-            Tarea.find({activo: true}).where(condicionPorVencer).populate('plaza', 'descripcion').sort({plaza: -1}), 
+            Tarea.find({activo: true})
+                 .skip(paginador.desdeTareas)
+                 .limit(paginador.hastaTareas)
+                 .populate('plaza', 'descripcion activo').sort({plaza: -1})        
+                 .where(condicionTareas),
+            
+            Tarea.find(busquedaVencidas)
+                 .skip(paginador.desdeVencidas)
+                 .limit(paginador.hastaVencidas)
+                 .where(condicionVencidas)
+                 .populate({path: 'plaza', select: 'descripcion activo'}).sort({plaza: -1}),         
+            
+            Tarea.find(busquedaPorVencer)
+                .skip(paginador.desdePorVencer)
+                .limit(paginador.hastaPorVencer)
+                .populate('plaza', 'descripcion activo').sort({plaza: -1})
+                .where(condicionPorVencer),
+            
             Tarea.find({activo: true}).where(condicionTareas).countDocuments(),
             Tarea.find({activo: true}).where(condicionPorVencer).countDocuments(),
             Tarea.find({activo: true}).where(condicionVencidas).countDocuments(),
+        
         ])  
         success(res, { 
             tareas, 
