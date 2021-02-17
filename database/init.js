70050d03f8865009ca9186d1d974fd6a086f7431
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-02-17 12:29:07 -0300

    Listo para desplegar

--- database/init.js
+++ database/init.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const brcrypt = require('bcryptjs');
 const chalk = require('chalk');
 const Usuario = require('../models/usuario.model'); 
+const Tipo = require('../models/tipo.model');
 
 // Funcion: Inicializacion de Base de datos
 const initialization = async () => {
@@ -14,31 +16,46 @@ const initialization = async () => {
             useFindAndModify: false              
         });
         console.log(chalk.green('[Equinoccio Technology]') + ' - Conexion a base de datos correcta');   
-        const data = {
-            dni: 'admin',
-            apellido: 'Admin',
-            nombre: 'Admin',
-            email: 'admin@gmail.com',
-            role: 'ADMIN_ROLE',
-            activo: true
-        }
-        
-        // Generacion de password encriptado
-        const salt = brcrypt.genSaltSync();
-        data.password = brcrypt.hashSync('admin', salt);
-
-        // Se crea y se almacena en la base de datos al usuario administrador
-        const usuario = new Usuario(data);
-        await usuario.save();
-        console.log(chalk.green('[Equinoccio Technology]') + ' - Usuario administrador creado');
-        console.log(chalk.green('[Equinoccio Technology]') + ' - Usuario: admin | Password: admin');
+        usuarioAdministrador();
+        tipoInicial();
         console.log(chalk.green('[Equinoccio Technology]') + ' - Inicializacion completada');
-
     }catch(err){
         console.log(err);
         throw new Error('Error al inicializar la base de datos');    
     }     
 } 
+
+const usuarioAdministrador = async () => {
+    const data = {
+        dni: 'admin',
+        apellido: 'Admin',
+        nombre: 'Admin',
+        email: 'admin@gmail.com',
+        role: 'ADMIN_ROLE',
+        activo: true
+    }
+    
+    // Generacion de password encriptado
+    const salt = brcrypt.genSaltSync();
+    data.password = brcrypt.hashSync('admin', salt);
+
+    // Se crea y se almacena en la base de datos al usuario administrador
+    const usuario = new Usuario(data);
+    await usuario.save();
+    console.log(chalk.green('[Equinoccio Technology]') + ' - Usuario administrador creado');
+    console.log(chalk.green('[Equinoccio Technology]') + ' - Usuario: admin | Password: admin');
+}
+
+const tipoInicial = async () => {
+    // Inicializando plaza
+    const plaza = new Tipo({ descripcion: 'Plaza' });
+    await plaza.save();
+    // Inicializando Boulevard
+    const boulevard = new Tipo({ descripcion: 'Boulevard' });
+    await boulevard.save();
+    console.log(chalk.green('[Equinoccio Technology]') + ' - Tipos inicializados');
+}
+
 
 // Ejecucion de la funcion
 initialization();
--- routes/tipos.routes.js
+++ routes/tipos.routes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-
 const { validarCampos } = require('../middleware/validar-campos');
 const { validarJWT } = require('../middleware/validar-jwt');
 
@@ -12,8 +11,13 @@ const {
 
 const router = Router();
 
-router.get('/', listarTipos);
-router.post('/', nuevoTipo);
-router.put('/', actualizarTipo);
+router.get('/', validarJWT, listarTipos);
+router.post('/', 
+    [
+        validarJWT,
+        check('descripcion', 'La descripción es obligatoria').not().isEmpty(),
+        validarCampos
+    ], nuevoTipo);
+router.put('/', validarJWT, actualizarTipo);
 
 module.exports = router;
